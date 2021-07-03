633efd21da3ef05d210f2c4039c794cdead13199
Vashti <patrena@sbcglobal.net>
2021-07-03 08:39:28 -0700

    updated city button

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
-//var searchTerm =document.querySelector("#searchTerm").value 
-//fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}id=cityid&appid=2cccf24439236510b904f1eae27b1030`)
-function weatherSearch(event) {
-    event.preventDefault()
+function weatherSearch(data) {
+    data.preventDefault()
 var searchTerm =document.querySelector("#searchTerm").value
 fetch (`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=2cccf24439236510b904f1eae27b1030&units=imperial`)
 .then(function(response){
@@ -11,8 +9,12 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=2c
 .then(function(response){
     console.log(response)
     console.log(response.main.temp)
+    console.log(response.sys.visibility)
  })
-
+ .then(function(response){
+    console.log(response)
+    console.log(response.list[40].wind)
+ })
  fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=2cccf24439236510b904f1eae27b1030&units=imperial`)
 .then(function(response){
     console.log("1st response")
@@ -21,64 +23,76 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=2c
 .then(function(response){
     console.log(response)
     console.log(response.list[32].main.temp)
- })
+})
+.then(function(response){
+    console.log(response)
+    console.log(response.list[40].wind)
+})
 }
-document.getElementById("searchBtn").addEventListener("click", weatherSearch);
+document.getElementById("searchBtn").addEventListener("click", weatherSearch)
 
-// var responseContainer=document.querySelector("#response-container")
+var responseContainer=document.querySelector("#response-container")
 
-// var text = document.createElement("text");
+var text = document.createElement("text");
 
-// var cityFormEl = document.querySelector("#city-form");
-// var cityInputEl = document.querySelector("#searchTerm");
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#searchTerm");
 
-// var formSubmitHandler = function(response){
-//     responset.preventDefault();
-//     console.log(response);  
-// };
-// cityFormEl.addEventListener("submit", formSubmitHandler);
+var formSubmitHandler = function(response){
+    responset.preventDefault();
+    console.log(response);  
+};
 
-// // get value from input element
-// var cityname = cityInputEl.value.trim();
+var formEl=document.querySelector("#search-form");
+// formEl.addEventListener("submit", function(event)=>{
+//     event.preventDefault();
+//     console.log("submitting form");
+var cityNameArr=[]
+var ls=JSON.parse(localStorage.getItem("city-List"));
+var cityBtns = document.querySelector("#city-btns");
 
-// if (cityname) {
-//   response(cityname);
-//   cityInputEl.value = "";
-// } else {
-//   alert("Please provide City name");
-// }
-// var displayResponse = function(city,searchTerm) {
-//     console.log(city);
-//     console.log(searchTerm);
-//     // loop over repos
-// for (var i = 0; i < city.length; i++) {
-//     // format repo name
-//     var cityName = city[i].value;
-  
-//     // create a container for each repo
-//     var cityEl = document.createElement("div");
-//     cityEl.classList = "list-item flex-row justify-space-between align-center";
-  
-//     // create a span element to hold repository name
-//     var titleEl = document.createElement("span");
-//     titleEl.textContent = cityName;
-  
-//     // append to container
-//     cityEl.appendChild(titleEl);
-  
-//     // append container to the dom
-//     cityContainerEl.appendChild(repoEl);
-//   }
-// }
-// var reponseContainerEl = document.querySelector("#response-container");
-// var citySearchTerm = document.querySelector("#city-search-term");
+if(!ls) {
+    console.log ("there is no localstorage");
+} else {cityNameArr.push();
+}    
+for (var i= 0; i < cityNameArr.length; i++) {
+    var newBtn = document.createElement("button");
+    newBtn.innerText = cityNameArr[i];
+    newBtn.addEventListener("click", function (event) {
+        console.log(event.target.innerText);
+    });
+    cityBtns.append(newBtn);
+}
+function submitform(event){
+    event.preventDefualt();
+    console.log("submitting form");
+var cityEl=document.querySelector("#city-name");
+var city=cityEl.value;
+console.log("city is:", city);    
+}
+// })
+// var cityBtn=["City"]
+// searchBtn.addEventListener("click", function () {
+//     cityBtn.classList.remove("hide")
+//     weatherSearch()
+// })
+// cityFormEl.addEventListener("weatherSearch", formSubmitHandler);
 
-// // function displayDate(){
-// //     var currentDay = document.querySelector("#currentDay")
-// //    var currentDate= moment();
-// //    currentDay.textContent = currentDate.format("dddd, MMMM Do YYYY")
-// //    return currentDay;
-// //     }
-// //     displayDate();
+// get value from input element
+var cityname = cityInputEl.value.trim();
 
-// //text.setAttribute('src', response.data[0].text.fixed_height.url);
+if (cityname) {
+  response(cityname);
+  cityInputEl.value = "";
+} else {
+  alert("Please provide City name");
+}
+// function displayDate(){
+//    var currentDay = document.querySelector("#currentDay")
+//    var currentDate= moment();
+//    currentDay.textContent = currentDate.format("L")
+//    return currentDay;
+//     }
+//     displayDate();
+
+// text.setAttribute('src', response.data[0].text.fixed_height.url);
